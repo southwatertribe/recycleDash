e5cc3104da231b2ef49c4bae2ec289da19120adc
southwatertribe <imdannycarter@gmail.com>
2023-09-07 18:10:32 -0700

    Remove

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ const startServer = async () => {
     //Use middleware
     server.use(
         cors({
-          origin: 'https://elaborate-piroshki-e49c3e.netlify.app/', 
+          origin: 'https://elaborate-piroshki-e49c3e.netlify.app', 
         //   'https://elaborate-piroshki-e49c3e.netlify.app/'
           // preflightContinue: true,
           // methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
